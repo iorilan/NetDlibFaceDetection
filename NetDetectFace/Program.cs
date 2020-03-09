e87b1e6c6370eccfb2cb6f1b73775674fa4696a7
lanliang <iorilan@hotmail.com>
2020-03-09 10:16:52 +0800

    a

---
+++ NetDetectFace/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DlibDotNet;
+using DlibDotNet.Extensions;
+using Dlib = DlibDotNet.Dlib;
+
+namespace NetDetectFace
+{
+    class Program{
+        private const string inputFilePath = "./TestImage/Michael-Jordan-Team-USA.png";
+
+        static void Main(string[] args)
+        {
+            // set up Dlib facedetector
+            using (var fd = Dlib.GetFrontalFaceDetector())
+            {
+                var img = Dlib.LoadImage<RgbPixel>(inputFilePath);
+
+// find all faces in the image
+                var faces = fd.Operator(img);
+                foreach (var face in faces)
+                {
+                    // draw a rectangle for each face
+                    Dlib.DrawRectangle(img, face, color: new RgbPixel(0, 255, 255), thickness: 4);
+                    Dlib.SaveJpeg(img, "output.jpg");
+                }
+            }
+        }
+    }
+    
+}
---
+++ readme.txt
@@ -0,0 +1,4 @@
+dotnet new console -o NetDetectFace
+cd .\NetDetectFace\
+dotnet add package DlibDotNet
+dotnet run
